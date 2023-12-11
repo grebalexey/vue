5af3ff21f04c3351e083b4ed490a105772c97b70
https://github.com/TigranPoghos <136621926+grebalexey@users.noreply.github.com>
2023-12-11 20:35:17 +0300

    vue начали учить

---
+++ app/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Card from './components/Card.vue'
+</script>
+<template>
+  <div class="card__title">Заголовок в app</div>
+  <Card title="Рубашка 1" :price="30000">
+    <button>Жмакни</button>
+  </Card>
+  <Card title="Рубашка 2" :price="31000 + 500" is-added="true" />
+  <Card title="Рубашка 3" price="32 000 + 500" :isAdded="true" />
+  <Card title="Рубашка 4" price="33 000" />
+  <Card title="Рубашка 5" price="34 000" />
+</template>
+<style>
+body {
+  background-color: black;
+  color: white;
+}
+</style>
---
+++ app/src/components/Card.vue
@@ -0,0 +1,52 @@
+<script setup>
+// // ООП подход
+// export default {
+//   data() {
+//     return {
+//       count: 0
+//     }
+//   },
+//   methods: {
+//     incriment() {
+//       this.count++
+//     }
+//   }
+// }
+
+// Композитный  подход
+import { ref } from 'vue'
+
+let count = ref(0)
+function incriment() {
+  count.value++
+}
+
+defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean
+})
+</script>
+
+<template>
+  <div class="card">
+    <div class="card__title">{{ title }}</div>
+    <div class="card__price">{{ price }} руб</div>
+    {{ isAdded }}
+
+    <slot></slot>
+    <!-- <h2>{{ count }}</h2>
+    <button @click="incriment">Добавить 1</button>
+    <input v-model="count" /> -->
+  </div>
+</template>
+
+<style scoped>
+/* body {
+  background-color: red;
+} */
+.card__title {
+  font-size: 20px;
+  color: aqua;
+}
+</style>
